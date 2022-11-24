c40f830af5686717a2b42ec6d2b84d7148fc432d
Fede <ferrelli.mazza@gmail.com>
2022-11-24 19:15:33 -0300

    Create react project with Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>Greydive Challenge</h1>
+        <a href="https://portafolio-fedeferrelli.vercel.app/" target="_blank">
+          Fede Mazza
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
