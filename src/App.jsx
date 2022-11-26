26783c53a8b8d550910db295557630ce8e15670a
Fede <ferrelli.mazza@gmail.com>
2022-11-26 19:09:52 -0300

    Style App padding

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,25 @@ import ShowTestDetail from "./components/showTestDetail/ShowTestDetail";
 function App() {
   return (
     <DataProvider>
-      <div className="bg-main text-secondary font-sans w-full min-h-screen px-4">
-        <div className="h-10 sm:h-20 text-lg font-semibold shadow-lg flex flex-row justify-between items-center">
+      <div className="bg-main text-secondary font-sans w-full min-h-screen">
+        <div className="h-16 sm:h-20 text-lg font-semibold shadow-lg flex flex-row justify-between items-center px-6 sm:px-8">
           <h1>Greydive Challenge</h1>
-          <a className="cursor-pointer hover:text-secondary/80 duration-300" href="https://portafolio-fedeferrelli.vercel.app/" target="_blank">
+          <a
+            className="cursor-pointer hover:text-secondary/80 duration-300"
+            href="https://portafolio-fedeferrelli.vercel.app/"
+            target="_blank"
+          >
             Fede Mazza
           </a>
         </div>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Links />} />
-            <Route path="/:testerNumberURL" element={<ShowTestDetail />} />
-          </Routes>
-        </Router>
+        <div className="px-6 lg:px-48">
+          <Router>
+            <Routes>
+              <Route path="/" element={<Links />} />
+              <Route path="/:testerNumberURL" element={<ShowTestDetail />} />
+            </Routes>
+          </Router>
+        </div>
       </div>
     </DataProvider>
   );
