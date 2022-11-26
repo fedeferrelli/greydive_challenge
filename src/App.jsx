3d18e96e345fd557975b31aa439c74aa391afbb6
Fede <ferrelli.mazza@gmail.com>
2022-11-26 17:32:48 -0300

    Style header

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,20 @@ import ShowTestDetail from "./components/showTestDetail/ShowTestDetail";
 function App() {
   return (
     <DataProvider>
-      <div>
-        <h1>Greydive Challenge</h1>
-        <a href="https://portafolio-fedeferrelli.vercel.app/" target="_blank">
-          Fede Mazza
-        </a>
+      <div className="bg-main text-secondary font-sans w-full min-h-screen px-4">
+        <div className="h-10 sm:h-20 text-lg font-semibold shadow-lg flex flex-row justify-between items-center">
+          <h1>Greydive Challenge</h1>
+          <a className="cursor-pointer hover:text-secondary/80 duration-300" href="https://portafolio-fedeferrelli.vercel.app/" target="_blank">
+            Fede Mazza
+          </a>
+        </div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Links />} />
+            <Route path="/:testerNumberURL" element={<ShowTestDetail />} />
+          </Routes>
+        </Router>
       </div>
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<Links />} />
-          <Route path="/:testerNumberURL" element={<ShowTestDetail />} />
-        </Routes>
-      </Router>
     </DataProvider>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        main: '#292643',
+        secondary: '#ffffff'
+      }
+    },
   },
   plugins: [],
 }
