1501a1c5ddc7d13ff109e4d48e871498c7d65e8f
Fede <ferrelli.mazza@gmail.com>
2022-11-26 18:41:24 -0300

    Style Transcription component

--- src/components/showTestDetail/Transcription.jsx
+++ src/components/showTestDetail/Transcription.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Transcription({ transcription, changeText }) {
   return (
-    <div>
-      <h1>Transcripción</h1>
-      <p style={{'whiteSpace':'pre-line'}}>{changeText(transcription)}</p>
+    <div className="mt-8">
+      <h1 className="font-semibold text-2xl">Transcripción</h1>
+      <p className="mt-8 whitespace-pre-line max-w-[600px] max-h-[300px] px-12 pb-15 overflow-scroll leading-6">{changeText(transcription)}</p>
     </div>
   );
 }
