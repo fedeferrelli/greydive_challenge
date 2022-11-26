ae3832c511b187c4d8f3a2c0409bec22fdf27764
Fede <ferrelli.mazza@gmail.com>
2022-11-26 18:27:22 -0300

    Style Video component

--- src/components/showTestDetail/Video.jsx
+++ src/components/showTestDetail/Video.jsx
@@ -1,7 +1,7 @@
 function Video({ linkVideo }) {
   return (
-    <div>
-      <video src={linkVideo} controls width="70%" height="70%" />
+    <div className="w-full max-w-[990px] m-auto py-3 mt-6 bg-black">
+      <video src={linkVideo} controls width="100%" />
     </div>
   );
 }
